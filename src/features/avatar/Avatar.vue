15d1acac65e195cec45980eae72b69a12d4e50e6
Naturean <112891509+Naturean@users.noreply.github.com>
2025-07-23 00:22:12 +0800

    completed main menu

---
+++ src/features/avatar/Avatar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+    <div class="w-10 rounded-full">
+      <img
+        src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/features/ticket/TicketForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <fieldset class="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+    <legend class="fieldset-legend">工单信息</legend>
+
+    <label class="label">上传者 *</label>
+    <input
+      type="text"
+      class="input w-auto"
+      placeholder="你的名字"
+      v-model="poster"
+      required
+    />
+
+    <label class="label">问题描述 *</label>
+    <textarea
+      class="textarea resize-none w-auto min-h-40 lg:min-h-50"
+      placeholder="详细描述问题"
+      v-model="description"
+      required
+    ></textarea>
+
+    <label class="label">上传图片</label>
+    <input type="file" class="file-input w-auto" accept="image/*" multiple />
+
+    <button class="btn my-4" @click="onSubmit">提交</button>
+  </fieldset>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const poster = ref("");
+const description = ref("");
+const image = ref("");
+
+function onSubmit() {
+  const issue = {
+    title: title.value,
+    submitor: poster.value,
+    description: description.value,
+    image: image.value,
+  };
+
+  console.log(issue);
+}
+</script>
+
+<style scoped></style>
